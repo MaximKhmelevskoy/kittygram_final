f1c1e0367a18ecac6edb76bc271811ff871a781d
Maxim Khmelevskoy <wowubivaet@gmail.com>
2026-05-10 10:41:46 +0300

    dfdfd

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,13 +6,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    "kittygram-maksi.duckdns.org",
+    "taski-maksi.duckdns.org",
     "localhost",
     "127.0.0.1",
     "backend",
-    "kittygram-maksi.duckdns.org",
+    "taski-backend",
+    "kittygram-backend",
 ]
 
 INSTALLED_APPS = [
@@ -61,8 +64,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'kittygram'),
+        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
