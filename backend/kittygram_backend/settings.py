f51d7e2790b29c480578ebfae9f6e4f7c1488e26
Maxim Khmelevskoy <wowubivaet@gmail.com>
2026-05-10 22:00:06 +0300

    dsfdfd

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,10 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    SECRET_KEY = get_random_secret_key()
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
